adafff283d154cdecf459afed9301d0503ee8b40
ich-liebe-dich-Elysia <3280354053@qq.com>
2025-06-25 16:22:10 +0800

    a_sum

--- Backend/workflows/Cleanwork.py
+++ Backend/workflows/Cleanwork.py
@@ -4,6 +4,7 @@ class CleanExecutor:
         self.nodes = self._clean_nodes()
         self.edges = self.raw_workflow.get('edges', [])
         self.nodes_with_next = self._add_next_to_nodes()
+        self.nodes_with_data = self._data_process()
     
     def _clean_nodes(self):
         """
@@ -29,20 +30,102 @@ class CleanExecutor:
         # 为每个节点添加next数组
         for node in self.nodes:
             node['next'] = []
+            if node['type']=='condition':
+                """
+                条件节点需要特殊处理，因为条件节点可以有多个输出
+    
+                """
+                # 查找所有以该节点为源节点的边
+                for edge in self.edges:
+                    if edge['sourceNodeID'] == node['id']:
+                        target_node = nodes_dict.get(edge['targetNodeID'])
+                        if target_node:
+                            node['next'].append({edge['sourcePortID']:target_node['id']})
+                continue
+
             # 查找所有以该节点为源节点的边
             for edge in self.edges:
                 if edge['sourceNodeID'] == node['id']:
                     target_node = nodes_dict.get(edge['targetNodeID'])
                     if target_node:
-                        node['next'].append(target_node['id'])
+                        node['next'].append({"next_id":target_node['id']})
         
         return self.nodes
 
+    def _data_process(self):
+        """
+        处理每个节点的data块，确保数据格式的一致性和完整性
+        """
+        for node in self.nodes_with_next:
+            # 确保每个节点都有data字段
+            if 'data' not in node:
+                node['data'] = {}
+            
+            # 根据节点类型处理data块
+            node_type = node.get('type', '')
+            data = node['data']
+            
+            # 为不同类型的节点设置默认data结构
+            if node_type == 'start':
+                if 'outputs' not in data:
+                    data['outputs'] = []
+            elif node_type == 'end':
+                if 'inputs' not in data:
+                    data['inputs'] = []
+            elif node_type in ['condition', 'loop', 'llm']:
+                if 'inputs' not in data:
+                    data['inputs'] = []
+                if 'outputs' not in data:
+                    data['outputs'] = []
+                if 'properties' not in data:
+                    data['properties'] = {}
+            
+            # 确保inputs和outputs是列表类型
+            for field in ['inputs', 'outputs']:
+                if field in data and not isinstance(data[field], list):
+                    data[field] = []
+            
+            # 确保properties是字典类型
+            if 'properties' in data and not isinstance(data['properties'], dict):
+                data['properties'] = {}
+        
+        return self.nodes_with_next
+
     def get_cleaned_workflow(self):
         """
-        获取清洗后的工作流数据，只返回带next数组的节点列表
+        获取清洗后的工作流数据，只返回带next数组的节点列表：
+
+        [
+    {
+        "id": "node1",
+        "type": "start",
+        "data": {...},
+        "next": {"next_id":"node2"}  # 指向的下一个节点ID
+    }
+    
+
+    ...
+]
+
+
+特例：condition节点需要特殊处理，因为条件节点可以有多个输出
+
+    条件节点需要特殊处理，因为条件节点可以有多个输出
+            {
+    "id": "condition_1",
+    "type": "condition",
+    "data": {
+        # 条件节点的其他数据
+    },
+    "next": {
+        "sourcePortId1": "node_id1",   
+        "sourcePortId2": "node_id2"  
+    }
+}
+              
         """
-        return self.nodes_with_next
+        print(self.nodes_with_data)
+        return self.nodes_with_data
 
 def clean_workflow(pre_workflow_data):
     """
--- Backend/workflows/Factory.py
+++ Backend/workflows/Factory.py
@@ -3,10 +3,14 @@ import logging
 logger = logging.getLogger(__name__)
 
 class NodeFactory:
-    def __init__(self,nodes):
-        self.nodes = nodes
+    def __init__(self, nodes):
+        # 将节点列表转换为以id为键的字典
+        self.nodes = {node['id']: node for node in nodes}
     
     def create_node_instance(self, nodeId):
+        if nodeId not in self.nodes:
+            logger.error(f"找不到ID为{nodeId}的节点")
+            return None
         return self.__create_node(self.nodes[nodeId]["type"], nodeId)
 
     def __create_node(self, type, nodeId):
@@ -14,10 +18,10 @@ class NodeFactory:
         根据节点类型创建节点。
 
         :param type: 字符串，表示节点的类型。
-        :param nodeId
+        :param nodeId: 节点的唯一标识符
         :return: 创建的节点对象。
 
-        创建节点的过程根据传入的 `node_type` 参数来判断。
+        创建节点的过程根据传入的 `type` 参数来判断。
         """
         match type:
             # case "start":
