dfb0b23a72b503ee5ade77ac0a69c327f33f90c1
ich-liebe-dich-Elysia <3280354053@qq.com>
2025-06-25 16:22:10 +0800

    a_sum

--- Backend/workflows/Engine.py
+++ Backend/workflows/Engine.py
@@ -2,26 +2,37 @@ import logging
 
 
 from Factory import NodeFactory
-from events import EventBus
+from events.EventBus import EventBus    
 
 
 class WorkflowEngine :
      
      def __init__(self, workflowData):
-          nodes = None 
-
+          # 从workflowData中获取处理后的节点数据
+          self.nodes = workflowData
+          
           self.bus = EventBus()
-          self.factory = NodeFactory(nodes)
-          self.startID = None
+          self.factory = NodeFactory(self.nodes)
+          # 查找并设置起始节点ID
+          self.startID = self._find_start_node()
           self.backStack = []
           self.instance = {}
 
           self.bus.on("askMessage", self.askMessage)
           self.bus.on("putStack", self.putStack)
 
-          pass
+     def _find_start_node(self):
+          """
+          在节点列表中查找类型为'start'的节点，并返回其ID
+          """
+          for node in self.nodes:
+               if node.get('type') == 'start':
+                    return node.get('id')
+          return None
 
      def run(self):
+          if self.startID is None:
+               return {"status": "error", "message": "找不到起始节点", "results": "返回代码1"}
           
           curNodeID = self.startID
           while curNodeID is not None:
@@ -40,8 +51,6 @@ class WorkflowEngine :
 
           return {"status" : "success", "message" : "蓝图运行成功", "results" : "返回代码0"}
      
-
-
      def askMessage(self, nodeId, nodePort):
           return self.instance[nodeId].getMessage(nodePort)
 
@@ -49,7 +58,7 @@ class WorkflowEngine :
           self.backStack.append(nodeID)
      
      def popStack(self):
-          if len(self.backStack) is 0:
+          if len(self.backStack) == 0:
                return None
           else:
                return self.backStack.pop()
