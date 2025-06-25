677b2680f5e96a8c148d9592e4d6975e52fd1992
ywtshuai <2287943335@qq.com>
2025-06-25 15:15:42 +0800

    更新next

--- Backend/workflows/Cleanwork.py
+++ Backend/workflows/Cleanwork.py
@@ -3,6 +3,7 @@ class CleanExecutor:
         self.raw_workflow = pre_workflow_data
         self.nodes = self._clean_nodes()
         self.edges = self.raw_workflow.get('edges', [])
+        self.nodes_with_next = self._add_next_to_nodes()
     
     def _clean_nodes(self):
         """
@@ -18,14 +19,30 @@ class CleanExecutor:
             cleaned_nodes.append(cleaned_node)
         return cleaned_nodes
     
+    def _add_next_to_nodes(self):
+        """
+        为每个节点添加next数组，表示指向的下一个节点
+        """
+        # 创建节点ID到节点的映射
+        nodes_dict = {node['id']: node for node in self.nodes}
+        
+        # 为每个节点添加next数组
+        for node in self.nodes:
+            node['next'] = []
+            # 查找所有以该节点为源节点的边
+            for edge in self.edges:
+                if edge['sourceNodeID'] == node['id']:
+                    target_node = nodes_dict.get(edge['targetNodeID'])
+                    if target_node:
+                        node['next'].append(target_node['id'])
+        
+        return self.nodes
+
     def get_cleaned_workflow(self):
         """
-        获取清洗后的工作流数据
+        获取清洗后的工作流数据，只返回带next数组的节点列表
         """
-        return {
-            'nodes': self.nodes,
-            'edges': self.edges
-        }
+        return self.nodes_with_next
 
 def clean_workflow(pre_workflow_data):
     """
