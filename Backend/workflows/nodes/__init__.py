0b1c3c4ce62f05786584ad2a4b12832caaec12a6
ywtshuai <2287943335@qq.com>
2025-06-30 09:51:34 +0800

    <feat>:pdf块后端实现

--- Backend/workflows/nodes/__init__.py
+++ Backend/workflows/nodes/__init__.py
@@ -6,9 +6,10 @@ from .Print import Print
 from .Loop import Loop
 from .End import End
 from .TextProcessor import TextProcessor
+from .PdfProcessor import PdfProcessor
 
 # 重命名导出的类，避免与模块名冲突
 
 __version__ = "1.0.0"
 
-__all__ = ["Start", "FileInput", "ConditionNode", "Print", "Loop", "End", "TextProcessor"]
+__all__ = ["Start", "FileInput", "ConditionNode", "Print", "Loop", "End", "TextProcessor", "PdfProcessor"]
